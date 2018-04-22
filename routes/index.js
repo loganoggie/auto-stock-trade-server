01f1c095fb69f20a8cd9a6f6236b049ce32932f6
Garuccio, Joseph S. (S&T-Student) <jsgkd6@mst.edu>
2018-04-22 17:29:00 -0500

    AAAAAA

--- routes/index.js
+++ routes/index.js
@@ -20,15 +20,55 @@ var pool = database.pool;
 /*Runs the correct algorithm for every investment.*/
 router.get('/run', function(req, res, next) {
 
-
-
-  /*
   queries.getAllInvestments("RSI",function(query) {
+    console.log("PROCESSING RSI ALGO");
     for(var i=0;i<query.rows.length;i++) //for each investment
     {
       if(query.rows[i].params=="low") //low risk RSI
       {
         //console.log("low");
+        var date = new Date();
+        var year = date.getFullYear();
+        var month = date.getMonth()+1;
+        var day = date.getDate()-2;
+
+        if(month<10)
+        {
+          month="0"+month;
+        }
+        if(day<10)
+        {
+          day="0"+day;
+        }
+
+
+      var stringDate = year+"-"+month+"-"+day; //converting the date into the string that AV wants
+        //https://www.alphavantage.co/query?function=RSI&symbol="+this.query.rows[this.i].stockticker+"&interval=daily&time_period=10&series_type=open&apikey=CJWPUA7R3VDJNLV0
+        request("https://www.alphavantage.co/query?function=RSI&symbol="+query.rows[i].stockticker+"&interval=daily&time_period=10&series_type=open&apikey=CJWPUA7R3VDJNLV0", function(error,response,body2)
+        {
+          console.log("RSIDate: "+stringDate);
+          var RSIvalue = JSON.parse(body2)['Technical Analysis: RSI'][stringDate]['RSI']; //this is the current price
+          //console.log("Current Price: "+currentPrice);
+          console.log("RSI Value: "+RSIvalue);
+          /*
+          if(currentPrice>RSIvalue)
+          {
+            queries.addNotification(this.query.rows[this.i].email,"User "+this.query.rows[this.i].email+" should sell "+this.query.rows[this.i].numstocks+" of "+this.query.rows[this.i].stockticker+" at a price of "+currentPrice+" each. This would make the investment worth $"+currentPrice*this.query.rows[this.i].numstocks+".",function(query)
+            {
+              //console.log("User "+query.rows[this.i].email+" should sell "+query.rows[this.i].numstocks+" of "+query.rows[this.i].stockticker+" at a price of "+this.currentPrice+" each. This would make the investment worth $"+this.currentPrice*query.rows[this.i].numstocks+".");//UnhandledPromiseRejection???
+              console.log("SELL THE STOCK:");
+            }.bind({ i: this.i, currentPrice: currentPrice}));
+          }
+          else
+          {
+            queries.addNotification(this.query.rows[this.i].email,"User "+this.query.rows[this.i].email+" should buy "+this.query.rows[this.i].stockticker+" at a price of "+currentPrice+" each.",function(query)
+            {
+              //console.log("User "+query.rows[this.i].email+" should buy "+query.rows[this.i].stockticker+" at a price of "+this.currentPrice+" each.");//UnhandledPromiseRejection???
+              console.log("BUY THE STOCK:");
+            }.bind({ i: this.i, currentPrice: currentPrice}));
+          }
+          */
+        }.bind({ query: query, i: i }));
       }
       else if(query.rows[i].params=="medium") //medium risk RSI
       {
@@ -44,21 +84,54 @@ router.get('/run', function(req, res, next) {
       }
     }
   });
-  */
 
   queries.getAllInvestments("MovingAverages",function(query) {
     for(var i=0;i<query.rows.length;i++) //for each investment
     {
-      var email = query.rows[i].email;
-      var numstocks = query.rows[i].numstocks;
-      var day = parseInt(query.rows[i].params); //day that the user passes in for moving averages
-      var stockTicker = query.rows[i].stockticker; //stock ticker
-      var apikey = req.session.userInfo.avkey;
+      var date = new Date();
+      var year = date.getFullYear();
+      var month = date.getMonth()+1;
+      var day = date.getDate()-1;
 
-      var request_array = ['SMA', stockTicker, 'daily', 2, apikey]
+      if(month<10)
+      {
+        month="0"+month;
+      }
+      if(day<10)
+      {
+        day="0"+day;
+      }
 
-      funcs.do_alpha_job(funcs.getTechnical, request_array, funcs.print_earliest)
+      var stringDate = year+"-"+month+"-"+day; //converting the date into the string that AV wants
 
+      console.log("Current date: "+stringDate);
+      request("https://www.alphavantage.co/query?function=SMA&symbol="+query.rows[i].stockticker+"&interval=daily&time_period=1"+parseInt(query.rows[i].params)+"&series_type=open&apikey=CJWPUA7R3VDJNLV0", function(error,response,body)
+      {
+        console.log("CHECK this stringdate:"+stringDate);
+        var movingAverageValue = JSON.parse(body)['Technical Analysis: SMA'][stringDate]['SMA']; //this is the moving average
+        request("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol="+this.query.rows[this.i].stockticker+"&interval=60min&apikey=CJWPUA7R3VDJNLV0", function(error,response,body2)
+        {
+          var currentPrice = JSON.parse(body2)['Time Series (60min)'][stringDate+' 16:00:00']['1. open']; //this is the current price
+          console.log("Current Price: "+currentPrice);
+          console.log("Moving Average Value: "+movingAverageValue);
+          if(currentPrice>movingAverageValue)
+          {
+            queries.addNotification(this.query.rows[this.i].email,"User "+this.query.rows[this.i].email+" should sell "+this.query.rows[this.i].numstocks+" of "+this.query.rows[this.i].stockticker+" at a price of "+currentPrice+" each. This would make the investment worth $"+currentPrice*this.query.rows[this.i].numstocks+".",function(query)
+            {
+              //console.log("User "+query.rows[this.i].email+" should sell "+query.rows[this.i].numstocks+" of "+query.rows[this.i].stockticker+" at a price of "+this.currentPrice+" each. This would make the investment worth $"+this.currentPrice*query.rows[this.i].numstocks+".");//UnhandledPromiseRejection???
+              console.log("SELL THE STOCK:");
+            }.bind({ i: this.i, currentPrice: currentPrice}));
+          }
+          else
+          {
+            queries.addNotification(this.query.rows[this.i].email,"User "+this.query.rows[this.i].email+" should buy "+this.query.rows[this.i].stockticker+" at a price of "+currentPrice+" each.",function(query)
+            {
+              //console.log("User "+query.rows[this.i].email+" should buy "+query.rows[this.i].stockticker+" at a price of "+this.currentPrice+" each.");//UnhandledPromiseRejection???
+              console.log("BUY THE STOCK:");
+            }.bind({ i: this.i, currentPrice: currentPrice}));
+          }
+        }.bind({ query: this.query, i: this.i }));
+      }.bind({ query: query, i: i }));
     }
   });
   res.render('splash');
@@ -119,40 +192,51 @@ router.get('/dashboard', function(req, res, next) {
 });
 
 router.get('/dash-get', function(req, res, next) {
-  //Example of how this ojbect should be constructed for the simeple dashboard graph. More info about that on the google Doc.
-  queries.getCurrentUserInfo(req.user.id, req.user.email, function(queryUser) {
-    req.session.userInfo=queryUser.rows[0];
-    req.session.worth_day = {
-      worth: [9000, 9200, 9460.43, 9750, 10000],
-      day: ["2-21-2018", "2-22-2018", "2-23-2018", "2-24-2018", "2-25-2018"]
-    };
-    req.session.total_worth = 10000.00;
-    res.json(JSON.stringify(req.session));
+
+  queries.getCurrentStockInfo(req.user.email, function(query){
+    req.session.stockInfo=query.rows;
+    queries.getNotifications(req.user.email, function(query2){
+      req.session.notifications=query2.rows;
+      queries.getCurrentUserInfo(req.user.id, req.user.email, function(queryUser) {
+        req.session.userInfo=queryUser.rows[0];
+        req.session.worth_day = {
+          worth: [9000, 9200, 9460.43, 9750, 10000],
+          day: ["2-21-2018", "2-22-2018", "2-23-2018", "2-24-2018", "2-25-2018"]
+          };
+        req.session.total_worth = 10000.00;
+        res.json(JSON.stringify(req.session));
+      });
+    });
   });
+
 });
 
 router.get('/tick-get', function(req, res, next) {
-  //Example of how this ojject should be constructed to generate tickers on the dashboard
-  queries.getCurrentUserInfo(req.user.id, req.user.email, function(queryUser) {
-    req.session.userInfo=queryUser.rows[0];
-    req.session.symbols=['GOOG', 'TSLA', 'AAPL', 'BA', 'AMD', 'BAC', 'BABA', 'EEP', 'EPD', 'JMP', 'GE', 'TWTR', 'FB', 'CHU'/*, 'TEP'*/]//An array of some of the most common stocks.
+  queries.getCurrentStockInfo(req.user.email, function(query){
+    req.session.stockInfo=query.rows;
     res.json(JSON.stringify(req.session));
   });
 });
 
 router.get('/investments-get', function(req, res, next) {
-  console.log(req.session);
-  res.json(JSON.stringify(req.session));
+    queries.getCurrentStockInfo(req.user.email, function(query){
+    req.session.stockInfo=query.rows;
+    res.json(JSON.stringify(req.session));
+  });
 });
 
 router.post('/add', function(req, res, next) {
 
   var tmp_param1 = 80;
   var tmp_param2 = 20;
-  var tmp_time_interval = '1min';
+  var time_interval = '1min';
+
+  var params = {}
 
   if (req.body.symbol == 'RSI')
   {
+    params.upper_bound = tmp_param1;
+    params.lower_bound = tmp_param2;
 
   }
   else if (req.body.symbol == 'BBANDS')
@@ -164,22 +248,15 @@ router.post('/add', function(req, res, next) {
 
   }
 
-  var params = {
-    param: tmp_param1,
-    param2: tmp_param2,
-    param3: req.body.radio,
-    param4: tmp_time_interval
-  }
+  // console.log(JSON.stringify(params))
 
-  client.query("INSERT INTO userstocks (email, stockticker, numstocks, algorithm, params, enabled) VALUES ('" + req.session.userInfo.email +
-    "','" + req.body.symbol + "','" + req.body.volume + "','" + req.body.algorithm + "','" + n.params + "','" + 1 + "')")
-
-  req.session.stockInfo.push(n)
+  client.query("INSERT INTO userstocks (email, stockticker, numstocks, algorithm, params, enabled) VALUES ('" + req.session.userInfo.email + 
+    "','" + req.body.symbol + "','" + req.body.volume + "','" + req.body.algorithm + "','" + req.body.radio + "','" + 1 + "')")
 
   console.log(req.session);
 
   res.render('investments')
-
+  
 });
 
 router.get('/investments', function(req, res, next) {
@@ -221,7 +298,7 @@ router.post('/updatePassword', async function(req, res, next) {
   var currentPassword = req['body']['currentPassword'];
   var newPassword = req['body']['newPassword'];
   var newPasswordConfirm = req['body']['newPasswordConfirm'];
-
+  
   console.log(req.session);
   console.log(req.session.userInfo);
   console.log(req.session.userInfo.password);
